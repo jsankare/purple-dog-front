44ad1c0b902ac71f2db90d263d4e73dc9260dcd4
Brubru <jordan.sankare@gmail.com>
2025-12-11 11:45:51 +0100

    rewrite nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = { 
-  /* config options here */
+const nextConfig: NextConfig = {
+    output: "standalone",
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://payload:3000/api/:path*',
+            },
+        ];
+    },
 };
 
 export default nextConfig;
