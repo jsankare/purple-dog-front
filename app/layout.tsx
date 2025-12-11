83a415b603347bdcb80e4c301148a7e48bd978cd
Aziz <mahmoudaziznasraoui@gmail.com>
2025-12-11 10:38:58 +0100

    removed dup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google"; 
 import "./globals.css";
-import type { Metadata } from "next";
 import Header from "@/app/components/layout/Header";
 import Footer from "@/app/components/layout/Footer";
 import Bot from "@/app/components/bot/bot";
